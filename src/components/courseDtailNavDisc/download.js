bb6e3df53ddbce3db7e6f09cd1db30cc28ad121b
Venktesh123 <8303531530venktesh@gmail.com>
2024-05-16 17:21:51 +0530

    added-pdf-url

--- src/components/courseDtailNavDisc/download.js
+++ src/components/courseDtailNavDisc/download.js
@@ -1,6 +1,8 @@
-// components/DownloadDropdown.js
-
 import { useState } from 'react';
+
+// URLs of the PDF files
+const hindiPdfUrl = 'https://www.dropbox.com/scl/fi/no6z8xgltckxz4lf77ybr/Commercial-Goat-Farming-Hindi.pdf?rlkey=vhb258xk2zo16vox46dbszp2b&st=p6wghkhl&dl=0';
+const englishPdfUrl = 'https://www.dropbox.com/scl/fi/vqyb4i3zs70r3d648jm2s/Commercial-Goat-Farming-English.pdf?rlkey=85ny5rm8dgof5vzd3ljwosuls&st=k8ruaib1&dl=0';
 
 const DownloadDropdown = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -10,9 +12,22 @@ const DownloadDropdown = () => {
   };
 
   const handleOptionClick = (language) => {
-    // Perform download action based on selected language
-    // For example, initiate a download or redirect to a download link
-    console.log(`Downloading PDF in ${language}`);
+    // Fetch PDF based on selected language
+    const url = language === 'Hindi' ? hindiPdfUrl : englishPdfUrl;
+    
+    // Create anchor element
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = `document_${language}.pdf`;
+    a.target = '_blank'; // Open in new tab
+    document.body.appendChild(a);
+
+    // Trigger download
+    a.click();
+
+    // Clean up
+    document.body.removeChild(a);
+
     // Close the dropdown after selecting an option
     setIsOpen(false);
   };
